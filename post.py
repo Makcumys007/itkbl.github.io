ecd99f53b0d091676abbd8887f2e91676527fd38
Maxim  Abylkassov <m.abylkassov@gmail.com>
2023-08-13 15:10:04 +0600

    last commit

--- post.py
+++ post.py
@@ -1,5 +1,10 @@
-from exchangelib import Credentials, Configuration, Account, DELEGATE
+
+from exchangelib import Configuration, GSSAPI, SSPIpin
+from exchangelib import Credentials, Configuration, Account, DELEGATE, GSSAPI, SSPI
 from exchangelib import Message, Mailbox
+from exchangelib.protocol import BaseProtocol
+BaseProtocol.USERAGENT = "Auto-Reply/0.1.0"
+
 import os
 
 LOGIN_PASSWORD = os.getenv("PASSWORD")
@@ -8,8 +13,13 @@ LOGIN_USER = os.getenv("LOGIN_USER")
 SMTP_SERVER = os.getenv("SMTP_SERVER") 
 RECEPIENT_EMAIL = os.getenv("RECEPIENT_EMAIL")
 
+
 credentials = Credentials(username=LOGIN_USER, password=LOGIN_PASSWORD)
+
+config = Configuration(auth_type=SSPI)
 config = Configuration(server=SMTP_SERVER, credentials=credentials)
+
+
 
 account = Account(
     primary_smtp_address=LOGIN_EMAIL,
