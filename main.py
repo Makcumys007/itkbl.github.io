8c342aca1ac4c7668081a4b5e080995b2d0218d3
Maxim  Abylkassov <m.abylkassov@gmail.com>
2023-08-14 17:26:23 +0600

    dotenv

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import os
 
 from exchangelib import Credentials, Configuration, Account, DELEGATE
 from exchangelib import Message, Mailbox
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+
 
 def send_email_to_recipients(subject, body):
 
--- post.py
+++ post.py
@@ -1,39 +1,37 @@
 from exchangelib import Credentials, Configuration, Account, DELEGATE, GSSAPI, SSPI, Build, NTLM, Version, IMPERSONATION, FaultTolerance
 from exchangelib import Message, Mailbox
-
-
-
+from dotenv import load_dotenv 
 import os
-if __name__ == '__main__':
-    LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
-    LOGIN_EMAIL = os.getenv("LOGIN_EMAIL")
-    LOGIN_USER = os.getenv("LOGIN_USER")    
-    SMTP_SERVER = os.getenv("SMTP_SERVER") 
-    RECEPIENT_EMAIL = os.getenv("RECEPIENT_EMAIL")
-    
-    
-    
-    
-    credentials = Credentials(username=LOGIN_USER, password=LOGIN_PASSWORD)
-    config = Configuration(server=SMTP_SERVER, credentials=credentials)
-    
-    
-    
-    account = Account(
-        primary_smtp_address=LOGIN_EMAIL,
-        config=config,
-        autodiscover=True,
-        access_type=DELEGATE,
-    )
-    
-    m = Message(
-        account=account,
-        folder= account.sent,
-        subject="Hello Message From Bot",
-        body="Hello! This is test message from bot",
-        to_recipients=[Mailbox(email_address=RECEPIENT_EMAIL)],
-    )
-    m.send_and_save()
+
+load_dotenv()
+
+
+LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
+LOGIN_EMAIL = os.getenv("LOGIN_EMAIL")
+LOGIN_USER = os.getenv("LOGIN_USER")    
+SMTP_SERVER = os.getenv("SMTP_SERVER") 
+RECEPIENT_EMAIL = os.getenv("RECEPIENT_EMAIL")
+
+credentials = Credentials(username=LOGIN_USER, password=LOGIN_PASSWORD)
+config = Configuration(server=SMTP_SERVER, credentials=credentials)
+
+
+
+account = Account(
+    primary_smtp_address=LOGIN_EMAIL,
+    config=config,
+    autodiscover=True,
+    access_type=DELEGATE,
+)
+
+m = Message(
+    account=account,
+    folder= account.sent,
+    subject="Hello Message From Bot",
+    body="Hello! This is test message from bot",
+    to_recipients=[Mailbox(email_address=RECEPIENT_EMAIL)],
+)
+m.send_and_save()
 
 
 
