6e39bbc29251106c0bd350f2e7f87328b075c94a
Maxim Abylkassov <m.abylkassov@gmail.com>
2023-12-17 09:50:43 +0600

    Add employeid for training

---
+++ training.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv 
+import os
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+from aiogram.dispatcher.filters import Command
+import time
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+import json 
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN2")
+
+# Установка уровня логирования
+logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# Обработчик команды /start
+@dp.message_handler(commands=['sba003'])
+async def start(message: types.Message):
+    await message.reply("Привет!")
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Табельный номер", web_app=WebAppInfo(url='https://passwordreset.microsoftonline.com/'))
+    markup.row(btn1)  
+    time.sleep(2)
+    await message.answer('service', reply_markup=markup)
+
+# Обработчик для всех остальных сообщений
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+# Запуск бота
+if __name__ == '__main__':
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
